a530c40e16f1405b849de83413f3a9470972e878
wille95 <sven.wille@kabelmail.de>
2021-11-15 20:07:18 +0100

    9er Reihe

--- Aufgabe_1.py
+++ Aufgabe_1.py
@@ -4,14 +4,14 @@
 # Schreiben Sie ein Programm, das mithilfe einer for-Schleife alle durch 9 teilbaren Zahlen 
 # zwischen zwei zuvor eingegebenen Grenzen in eine Liste schreibt und dann ausgibt.
 
-x = int(input("Neunerreihe zwischen Zahl 1: ")) 
-y = int(input("und Zahl 2: "))
-Neunerreihe = []
+x = int(input("Neunerreihe zwischen Zahl 1: ")) # Untere Grenze Neunerreihe
+y = int(input("und Zahl 2: "))                  # Obere Grenze Neunerreihe
+Neunerreihe = []                                # Leere Liste
 
-for i in range (x,y+1):  
-    if i % 9 == 0:
-        Neunerreihe += [i]
+for i in range (x,y+1):                         # Schleife um alle Zahlen in der angegeben Range  
+    if i % 9 == 0:                              # durch 9 teilbar sind
+        Neunerreihe += [i]                      # in Liste schreiben
 
-for i in range (0,i+1): 
+for i in range (0,i+1):                         # Liste ausgeben
     print(Neunerreihe[i])
 
